1fc36741b0447bdd9538463e25805bb0ee0c6280
HundredPercentCoverage <bunglenutter@gmail.com>
2022-04-30 23:11:06 +0100

    Registration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import { User } from "@prisma/client";
 import { withIronSessionSsr } from "iron-session/next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { sessionOptions } from "../lib/session";
 
-const Home = ({ user }: { user: User }) => {
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const router = useRouter();
 
   const register = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
 
     const res = await fetch("/api/register", {
       body: JSON.stringify({
@@ -23,11 +25,22 @@ const Home = ({ user }: { user: User }) => {
     });
 
     const result = await res.json();
-    console.log(result);
+
+    if (res.ok) {
+      console.log('registered');
+    } else {
+      const error = result.message.includes('username') 
+        ? 'User already exists'
+        : result.message;
+
+      setError(error);
+      setIsLoading(false);
+    }
   };
 
   const login = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
 
     const res = await fetch("/api/login", {
       body: JSON.stringify({
@@ -42,9 +55,13 @@ const Home = ({ user }: { user: User }) => {
 
     const result = await res.json();
 
-    if (result.ok) {
+    if (res.ok) {
       router.push("/dashboard");
+    } else {
+      setError(result.message);
     }
+    
+    setIsLoading(false);
   };
 
   return (
@@ -55,6 +72,9 @@ const Home = ({ user }: { user: User }) => {
       </Head>
 
       <main>
+        {error.length > 0 && (
+          <h2 className="w-full p-2 text-xl font-semibold text-white bg-red-500">{error}</h2>
+        )}
         <form onSubmit={register} className="mb-6">
           <h2 className="text-xl font-bold">Register</h2>
           <div className="flex flex-col items-start space-y-4">
@@ -66,7 +86,13 @@ const Home = ({ user }: { user: User }) => {
               <span>Password</span>
               <input type="password" name="password" id="password" className="p-2 border-2 border-gray-200 rounded-md focus:outline-none focus:ring ring-offset-2 ring-orange-400" required />
             </label>
-            <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md" type="submit">Register</button>
+            <button
+              className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+              type="submit"
+              disabled={isLoading}
+            >
+              Register
+            </button>
           </div>
         </form>
         <form onSubmit={login} className="mb-6">
@@ -80,7 +106,13 @@ const Home = ({ user }: { user: User }) => {
               <span>Password</span>
               <input type="password" name="password" id="password" className="p-2 border-2 border-gray-200 rounded-md focus:outline-none focus:ring ring-offset-2 ring-orange-400" required />
             </label>
-            <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md" type="submit">Log In</button>
+            <button
+              className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+              type="submit"
+              disabled={isLoading}
+            >
+              Log In
+            </button>
           </div>
         </form>
       </main>
